dffd5ab5fcf407b407e3f00aa21a33ee3fc98930
Ashley Chapman <ash-bash32@hotmail.co.uk>
2020-08-09 16:34:57 +0100

    Fixed a RGB Component Bug

--- Sources/ColorKit/Extensions/Color.swift
+++ Sources/ColorKit/Extensions/Color.swift
@@ -554,17 +554,23 @@ public extension Color {
     
     public func toRGBAComponents() -> (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
         
-        let scanner = Scanner(string: self.description.trimmingCharacters(in: CharacterSet.alphanumerics.inverted))
-        var hexNumber: UInt64 = 0
-        var r: CGFloat = 0.0, g: CGFloat = 0.0, b: CGFloat = 0.0, a: CGFloat = 0.0
-
-        let result = scanner.scanHexInt64(&hexNumber)
-        if result {
-            r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-            g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-            b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-            a = CGFloat(hexNumber & 0x000000ff) / 255
-        }
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        
+        #if os(iOS) || os(tvOS) || os(watchOS)
+        r = self.toUIColor().components().r
+        g = self.toUIColor().components().g
+        b = self.toUIColor().components().b
+        a = self.toUIColor().components().a
+        #elseif os(OSX)
+        r = self.toNSColor().components().r
+        g = self.toNSColor().components().g
+        b = self.toNSColor().components().b
+        a = self.toNSColor().components().a
+        #endif
+        
         return (r, g, b, a)
     }
     
